b4fafdd2df2bafe42e6d4202954a6068a067678b
Ruban Prakash <rubanprakash@hotmail.com>
2022-11-12 17:30:03 +0000

    add popup button for notification

---
+++ components/MarkPopup.vue
@@ -0,0 +1,28 @@
+<template>
+  <div @click="openPopup">
+    <img src="../assets/icons/dots.svg" alt="">
+  </div>
+</template>
+
+<script setup>
+function openPopup() {
+  // do stuff
+}
+</script>
+
+<style scoped>
+div {
+  cursor: pointer;
+  background-color: lightgray;
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  padding-top: 3px;
+}
+
+img {
+  width: 24px;
+  height: 24px;
+  margin: auto;
+}
+</style>
