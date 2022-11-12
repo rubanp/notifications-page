588b668bd9aba78eed621d1dace38bceb0e4b77e
Ruban Prakash <rubanprakash@hotmail.com>
2022-11-12 17:29:23 +0000

    add page wrapper

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,34 @@
 <template>
   <main>
-    <NotificationPopup />
+    <div class="wrapper">
+      <NotificationPopup />
+    </div>
   </main>
 </template>
+
+<style scoped>
+main {
+  width: 50%;
+  display: flex;
+  justify-content: center;
+}
+
+@media only screen and (max-width: 1200px) {
+  main {
+    width: 75%;
+  }
+}
+
+@media only screen and (max-width: 800px) {
+  main {
+    width: 85%;
+  }
+}
+
+@media only screen and (max-width: 600px) {
+  main {
+    width: 95%;
+  }
+}
+
+</style>
