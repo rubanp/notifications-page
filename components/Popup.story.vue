b93d6125801093a82dd6027bf7809a6a5f286ff7
Ruban Prakash <rubanprakash@hotmail.com>
2022-11-13 16:24:57 +0000

    add popup to toggle read and remove notification

---
+++ components/Popup.story.vue
@@ -0,0 +1,5 @@
+<template>
+  <Story title="Notification Popup">
+    <Popup />
+  </Story>
+</template>
---
+++ components/Popup.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="popup-card">
+    <img src="../assets/notification-popup.svg" alt="" class="popup-svg">
+    <div class="popup-content">
+      <div class="action">
+        <img class="icon" src="../assets/icons/tick.svg" alt="">
+        <p>Mark as {{ status }}</p>
+      </div>
+      <div class="action">
+        <img class="icon" src="../assets/icons/remove.svg" alt="">
+        <p>Remove</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const status = 'read'
+</script>
+
+<style scoped>
+
+img {
+  margin-right: 0.75em;
+}
+
+.popup-card {
+  position: relative;
+}
+
+.popup-content {
+  position: absolute;
+  top: 20px;
+  left: 12px;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5em;
+}
+
+.icon {
+  display: inline-block;
+}
+
+.action {
+  display: flex;
+  border-radius: 5px;
+}
+
+.action:hover {
+  cursor: pointer;
+  opacity: 0.6;
+}
+</style>
